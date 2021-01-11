5bd1db7c478a8f744dc2988a77a30d0ea348d3b3
SEM-DEV Inc <oualid909@gmail.com>
2021-01-11 17:52:17 +0300

    Update popup.js

--- mozilla addons/popup.js
+++ mozilla addons/popup.js
@@ -21,53 +21,163 @@ function handler() {
         }
     }
 }
-window.onload = function() {
-  var input = document.getElementById("myInput").focus();
-}
-window.onload = function() {
-  var input = document.getElementById("myInput").focus();
-}
-// Get the saved stats and render the data in the popup window.
-const MAX_ITEMS = 5;
-
-function sorter(array) {
-  return Object.keys(array).sort((a, b) => {
-    return array[a] <= array[b];
-  });
-}
-
-function addElements(element, array, callback) {
-  while(element.firstChild) {
-    element.removeChild(element.firstChild);
-  }
-
-  for (let i=0; i < array.length; i++) {
-    if (i >= MAX_ITEMS) {
-      break;
-    }
-
-    const listItem = document.createElement("li");
-    listItem.textContent = callback(array[i]);
-    element.appendChild(listItem);
-  }
-}
-
-var gettingStoredStats = browser.storage.local.get();
-gettingStoredStats.then(results => {
-  if (results.type.length === 0 || results.host.length === 0) {
-    return;
-  }
-
-  let hostElement = document.getElementById("hosts");
-  let sortedHosts = sorter(results.host);
-  addElements(hostElement, sortedHosts, (host) => {
-    return `${host}: ${results.host[host]} visit(s)`;
-  });
-
-  let typeElement = document.getElementById("types");
-  let sortedTypes = sorter(results.type);
-  addElements(typeElement, sortedTypes, (type) => {
-    return `${type}: ${results.type[type]} use(s)`;
-  });
-
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("myInput1").addEventListener("keyup", handlera);
 });
+
+// The handler also must go in a .js file
+function handlera() {
+     var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput1");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("showthelongtext");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+window.onload = function() {
+  var input = document.getElementById("myInput").focus();
+ 
+}
+function isValidURL(string) {
+  var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+  return (res !== null)
+};
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("addb").addEventListener("click", handler1);
+});
+
+
+function handler1() {
+	var linkaa=document.getElementById("link").value;
+	var titlea=document.getElementById("title").value;
+		
+
+	
+	
+		if ( titlea == "" || linkaa == "" ) {
+		    alert('please type the link and the title');
+		    document.getElementById("link").value="";
+            document.getElementById("title").value="";
+	}
+		else {
+		if ( isValidURL(linkaa) == true) {
+			var link1=document.getElementById("link").value;
+            var title1=document.getElementById("title").value;
+            document.getElementById("showthelongtext").innerHTML=document.getElementById("showthelongtext").innerHTML + "<li class='linka1'><a class='linka' href='" + link1 +"'>" + title1 +"</a></li>";
+            var dech=document.getElementById("showthelongtext").innerHTML;
+            document.getElementById("link").value="";
+            document.getElementById("title").value="";
+            localStorage.setItem("dfh4", dech);
+		}
+		else {
+			alert('please type a correct link');
+		    document.getElementById("link").value="";
+            
+			
+		}
+			
+		}
+		
+
+
+}
+
+window.addEventListener('load', (event) => {
+    if(localStorage.getItem("dfh4")!="") { 
+ var dech = localStorage.getItem("dfh4"); 
+ document.getElementById("showthelongtext").innerHTML=dech;
+   }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("cleartext").addEventListener("click", handler3);
+});
+
+
+function handler3() {
+document.getElementById("showthelongtext").innerHTML="";
+var dech=document.getElementById("showthelongtext").innerHTML;
+localStorage.setItem("dfh4", dech);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("openexist").addEventListener("click", handler4);
+});
+
+
+function handler4() {
+document.getElementById("exist1").style.display="block";
+document.getElementById("stored1").style.display="none";
+document.getElementById("openexist").style.backgroundColor="#0d3457";
+document.getElementById("openstored").style.backgroundColor="#0d1117";
+ var input = document.getElementById("myInput").focus();
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("openstored").addEventListener("click", handler5);
+});
+
+
+function handler5() {
+document.getElementById("exist1").style.display="none";
+document.getElementById("openadd").style.backgroundColor="#0d1117";
+ document.getElementById("openadd").innerHTML="Click to add  more links";
+document.getElementById("stored1").style.display="block";
+document.getElementById("addinput").style.display="none";
+document.getElementById("openstored").style.backgroundColor="#0d3457";
+document.getElementById("openexist").style.backgroundColor="#0d1117";
+ var input = document.getElementById("myInput1").focus();
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("openadd").addEventListener("click", handler6);
+});
+
+
+function handler6() {
+	var disp=document.getElementById("addinput").style.display;
+	if ( disp == "none") {
+document.getElementById("addinput").style.display="block";
+document.getElementById("link").value="";
+document.getElementById("title").value="";
+document.getElementById("openadd").style.backgroundColor="#0d3457";
+document.getElementById("openadd").innerHTML="Click to close the input box";
+ var input = document.getElementById("link").focus();
+	}
+	if ( disp == "block") {
+document.getElementById("addinput").style.display="none";
+document.getElementById("openadd").style.backgroundColor="#0d1117";
+ document.getElementById("openadd").innerHTML="Click to add  more links";
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
