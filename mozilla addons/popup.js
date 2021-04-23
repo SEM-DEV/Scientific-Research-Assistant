4600579b2efc8e484180e7ece604012d6932c6a5
SEM-DEV Inc <oualid909@gmail.com>
2021-04-24 02:18:44 +0300

    Update popup.js

--- mozilla addons/popup.js
+++ mozilla addons/popup.js
@@ -127,6 +127,36 @@ window.addEventListener('load', (event) => {
  var dech = localStorage.getItem("dfh4"); 
  document.getElementById("showthelongtext").innerHTML=dech;
    }
+   var list, i, switching, b, shouldSwitch;
+  list = document.getElementById("showthelongtext");
+  switching = true;
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // start by saying: no switching is done:
+    switching = false;
+    b = list.getElementsByTagName("LI");
+    // Loop through all list-items:
+    for (i = 0; i < (b.length - 1); i++) {
+      // start by saying there should be no switching:
+      shouldSwitch = false;
+      /* check if the next item should
+      switch place with the current item: */
+      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+        /* if next item is alphabetically
+        lower than current item, mark as a switch
+        and break the loop: */
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark the switch as done: */
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
+  }
 });
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -195,6 +225,7 @@ document.getElementById("addinput").style.display="none";
 document.getElementById("openstored").style.backgroundColor="#0d3457";
 document.getElementById("openexist").style.backgroundColor="#0d1117";
  var input = document.getElementById("myInput1").focus();
+ 
 }
 
 
