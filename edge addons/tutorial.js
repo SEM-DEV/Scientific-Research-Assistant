54ab4a6f464867185002325475e8175e4998a1fd
oualid semmana <oualid909@gmail.com>
2024-06-09 15:53:38 +0300

    Update tutorial.js

--- edge addons/tutorial.js
+++ edge addons/tutorial.js
@@ -36,3 +36,63 @@ document.addEventListener('DOMContentLoaded', function() {
 function next() {
 plusSlides(1);
 }
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("currentSlide1").addEventListener("click", currentSlide1);
+});
+function currentSlide1() {
+currentSlide(1);
+}
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("currentSlide2").addEventListener("click", currentSlide2);
+});
+function currentSlide2() {
+currentSlide(2);
+}
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("currentSlide3").addEventListener("click", currentSlide3);
+});
+function currentSlide3() {
+currentSlide(3);
+}
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("currentSlide4").addEventListener("click", currentSlide4);
+});
+function currentSlide4() {
+currentSlide(4);
+}
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("currentSlide5").addEventListener("click", currentSlide5);
+});
+function currentSlide5() {
+currentSlide(5);
+}
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("currentSlide6").addEventListener("click", currentSlide6);
+});
+function currentSlide6() {
+currentSlide(6);
+}
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("currentSlide7").addEventListener("click", currentSlide7);
+});
+function currentSlide7() {
+currentSlide(7);
+}
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("currentSlide8").addEventListener("click", currentSlide8);
+});
+function currentSlide8() {
+currentSlide(8);
+}
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("currentSlide9").addEventListener("click", currentSlide9);
+});
+function currentSlide9() {
+currentSlide(9);
+}
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("currentSlide10").addEventListener("click", currentSlide10);
+});
+function currentSlide10() {
+currentSlide(10);
+}
